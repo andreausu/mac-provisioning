50cdfa03063c1f47a47fed71291ff4e37747fd99
Andrea Usuelli <andrea.usuelli@prima.it>
2021-05-18 22:34:22 +0200

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ -z "$1" ]; then
 	echo "You must provide a role: ./run.sh home|work"
 fi
@@ -7,11 +9,16 @@ fi
 # Ask for the administrator password upfront
 sudo -v
 
+sudo mkdir -p /usr/local/bin
+sudo chown andrea /usr/local/bin
+
 # Keep-alive sudo until `clenaup.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-if [ ! -f /usr/local/bin/brew ]; then
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [ ! -f /opt/homebrew/bin/brew ]; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 if [ ! -f /usr/local/bin/ansible ]; then
@@ -22,7 +29,7 @@ if [ ! -f ~/.zshrc ]; then
 	curl -L http://install.ohmyz.sh | sh
 fi
 
-/usr/local/bin/ansible-galaxy install -r galaxy-roles.yml
-/usr/local/bin/ansible-playbook -i local -e role=$1 playbook.yml -v -K
+ansible-galaxy install -r galaxy-roles.yml
+ansible-playbook -i local -e role=$1 playbook.yml -v -K
 
-bash files/macos-settings.sh
+files/macos-settings.sh
